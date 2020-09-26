2f35088b6a4fc9fa55e16c36239b53bbca3fc013
Asimanand1104 <68984932+Asimanand1104@users.noreply.github.com>
2020-09-26 07:22:08 +0530

    Update Main.java

--- Lesson1/Control Flow/Control Flow (video 1)/if-then-statement/Main.java
+++ Lesson1/Control Flow/Control Flow (video 1)/if-then-statement/Main.java
@@ -1,15 +1,18 @@
 /**
 * Main class of the Java Program
 */
-public class Main{
-	public static void main(String[] args) {
+public class Main
+{
+	public static void main(String[] args)
+        {
 		int grade =85;
-		if(grade > 70){
+		if(grade > 70)
+                {
 			System.out.println("Congratulations!");
-		}else{
+		}
+                else
+                {
 			System.out.println("You should work harder");
 		}
-			
-				
 	}
 }
